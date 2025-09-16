5fd0b2801a46f5fd3089a36785a8989b00652540
Amber-222 <ST10287325@vcconnect.edu.za>
2025-09-16 14:08:37 +0200

    basic set up in place
    * added customer class for input fields
    * folder for customer views added
    * controller for customer data added
    * basic form for create new added

---
+++ ArkSoft_MVC/ArkSoft_MVC/Controllers/CustomerController.cs
@@ -0,0 +1,31 @@
+﻿using ArkSoft_MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ArkSoft_MVC.Controllers
+{
+    public class CustomerController : Controller
+    {
+        //METHOD TO RETURN PAGE THAT SHOWS LIST OF ALL CUSTOEMRS IN DB
+        public IActionResult AllCustomers()
+        {
+            //pull a list of customer data in db and return list
+            return View();
+        }
+
+        //METHOD TO RETURN PAGE THAT SHOWS FORM TO ADD A NEW CUSTOMER
+        public IActionResult CreateCustomer()
+        {
+            //displays add page for form
+            return View();
+        }
+
+        //METHOD TO ADD A NEW CUSTOMER
+        public IActionResult AddCustomer(Customer newCust)
+        {
+            //process customer object from form
+            //add to db
+            //redirect to page that shows list of customers to show updated list
+            return View("AllCustomers");
+        }
+    }
+}
---
+++ ArkSoft_MVC/ArkSoft_MVC/Models/Customer.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ArkSoft_MVC.Models
+{
+    public class Customer
+    {
+        [Key]
+        public int custID { get; set; } //keeps records unique
+        public string custName { get; set; } //stores full name
+        public string custAddress { get; set; }
+        public string custTelephone { get; set; }
+        public string custContactName { get; set; } //contact person's full name
+        public string custContactEmail { get; set; } //contact person's email VALID EMAIL ONLY
+        public int vatNumber { get; set; }
+
+        //empty constructor
+        public Customer()
+        {
+
+        }
+
+        //constructor to create objects
+        public Customer(int custID, string custName, string custAddress, string custTelephone, string custContactName, string custContactEmail, int vatNumber)
+        {
+            this.custID = custID;
+            this.custName = custName;
+            this.custAddress = custAddress;
+            this.custTelephone = custTelephone;
+            this.custContactName = custContactName;
+            this.custContactEmail = custContactEmail;
+            this.vatNumber = vatNumber;
+        }
+    }
+}
